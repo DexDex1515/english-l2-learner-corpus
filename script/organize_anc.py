2f2783d4173209bec43918c696b7640e59ec814c
hollyyuqizheng <holly_zyq@126.com>
2020-10-26 10:01:19 -0400

    Update organize script with frequency columns

--- script/organize_anc.py
+++ script/organize_anc.py
@@ -4,12 +4,16 @@ import os
 import csv
 from pdb import set_trace as bp
 
+# download the English tokenizer from NLTK
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-DATA_DIR = "../data/OANC-GrAF/data/written_2/technical/plos_txt/"
 
-csv_file = open('plos.csv', 'w', newline='')
+DATA_DIR = "../data/OANC-GrAF/data/written_1/journal/verbatim_txt/"
+CSV_FILE_NAME = 'verbatim.csv'
+
+csv_file = open(CSV_FILE_NAME, 'w', newline='')
 csv_writer = csv.writer(csv_file, delimiter=',')
-csv_writer.writerow(['file_id', 'num_sent', 'count_the', 'count_this', 'count_that', 'count_these', 'count_those', 'count_a', 'count_an', 'count_one'])
+csv_writer.writerow(
+    ['file_id', 'num_sent', 'word_count', 'count_the', 'count_this', 'count_that', 'count_these', 'count_those', 'count_a', 'count_an', 'count_one', 'freq_the', 'freq_this', 'freq_that', 'freq_these', 'freq_those', 'freq_a', 'freq_an', 'freq_one'])
 
 with os.scandir(DATA_DIR) as all_files:
     for entry in all_files:
@@ -19,7 +23,7 @@ with os.scandir(DATA_DIR) as all_files:
         #print(tokenized_sentences)
         num_sent = len(tokenized_sentences)
 
-        word_freq = {
+        word_count = {
             "the" : 0,
             "this" : 0,
             "that" : 0,
@@ -30,12 +34,30 @@ with os.scandir(DATA_DIR) as all_files:
             "one" : 0
         }
 
+        all_word_count = 0
+
         for sentence in tokenized_sentences:
-            for word in sentence.split():
-                cleaned_word = word.lower().strip(",.!?#'\"")
-                if cleaned_word in word_freq:
-                    word_freq[cleaned_word] += 1
-        csv_writer.writerow([entry.name, num_sent] + list(word_freq.values()))
+            sentences = sentence.split()
+            all_word_count += len(sentences)
+            for word in sentences:
+                cleaned_word = word.lower().strip(",.!?#()[]{}'\"")
+                if cleaned_word in word_count:
+                    word_count[cleaned_word] += 1
+
+        word_freq = {
+            "the" : 0.0,
+            "this" : 0.0,
+            "that" : 0.0,
+            "these" : 0.0,
+            "those" : 0.0,
+            "a" : 0.0, 
+            "an" : 0.0, 
+            "one" : 0.0
+        }
+        for word in word_freq:
+            word_freq[word] = word_count[word] / all_word_count 
+
+        csv_writer.writerow([str(entry.name).strip('.txt'), num_sent, all_word_count] + list(word_count.values()) + list(word_freq.values()))
 
 csv_file.close()
 
